7a0ca5a223f889130c34826a52f033263c0daa8e
erlantzroyo46-ui <erlantzroyo46@gmail.com>
2026-01-19 22:46:09 +0100

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,37 @@ import os
 
 app = Flask(__name__)
 
-# LISTA MASIVA COMPLETA - PAQUETE PREMIUM TOTAL
-canales_data = [
-    # --- FÚTBOL (LALIGA, CHAMPIONS, MULTIS) ---
+# LISTA MAESTRA TOTAL (MÁS DE 180 CANALES)
+CANALES = [
+    # --- TDT NACIONAL (EN ABIERTO) ---
+    {"n": "La 1", "id": "TVE", "g": "TDT"}, {"n": "La 2", "id": "TVE2", "g": "TDT"},
+    {"n": "Antena 3", "id": "ANTENA3", "g": "TDT"}, {"n": "Cuatro", "id": "CUATRO", "g": "TDT"},
+    {"n": "Telecinco", "id": "TELECINCO", "g": "TDT"}, {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
+    {"n": "FDF", "id": "FDF", "g": "TDT"}, {"n": "Energy", "id": "ENERGY", "g": "TDT"},
+    {"n": "Divinity", "id": "DIVINITY", "g": "TDT"}, {"n": "Mega", "id": "MEGA", "g": "TDT"},
+    {"n": "BeMad", "id": "BEMAD", "g": "TDT"}, {"n": "Atreseries", "id": "ATRESERIES", "g": "TDT"},
+    {"n": "Trece", "id": "TRECE", "g": "TDT"}, {"n": "DMAX", "id": "DMAX", "g": "TDT"},
+    {"n": "GOL PLAY", "id": "GOL", "g": "TDT"}, {"n": "DKISS", "id": "DKISS", "g": "TDT"},
+    {"n": "Clan", "id": "CLAN", "g": "Infantil"}, {"n": "Boing", "id": "BOING", "g": "Infantil"},
+
+    # --- AUTONÓMICOS ---
+    {"n": "Canal Sur", "id": "CSUR", "g": "Autonómicos"}, {"n": "TV3", "id": "TV3", "g": "Autonómicos"},
+    {"n": "ETB1", "id": "ETB1", "g": "Autonómicos"}, {"n": "ETB2", "id": "ETB2", "g": "Autonómicos"},
+    {"n": "Telemadrid", "id": "TMADRID", "g": "Autonómicos"}, {"n": "TVG", "id": "TVG", "g": "Autonómicos"},
+    {"n": "A Punt", "id": "APUNT", "g": "Autonómicos"}, {"n": "TV Canaria", "id": "TVCAN", "g": "Autonómicos"},
+
+    # --- FÚTBOL (LALIGA EA SPORTS Y HYPERMOTION) ---
     {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
     {"n": "M. LALIGA TV 2", "id": "LALIGA2", "g": "Fútbol"},
     {"n": "M. LALIGA TV 3", "id": "LALIGA3", "g": "Fútbol"},
     {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
     {"n": "DAZN LALIGA 2", "id": "DAZNLALIGA2", "g": "Fútbol"},
+    {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
+    {"n": "LaLiga Hypermotion 2", "id": "HYPERMOTION2", "g": "Fútbol"},
+    {"n": "LaLiga Hypermotion 3", "id": "HYPERMOTION3", "g": "Fútbol"},
+    {"n": "LaLiga Hypermotion 4", "id": "HYPERMOTION4", "g": "Fútbol"},
+    
+    # --- LIGA DE CAMPEONES Y EUROPA ---
     {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
     {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
     {"n": "M. Liga de Campeones 3", "id": "CHAMPIONS3", "g": "Champions"},
@@ -19,51 +42,50 @@ canales_data = [
     {"n": "M. Liga de Campeones 6", "id": "CHAMPIONS6", "g": "Champions"},
     {"n": "M. Liga de Campeones 7", "id": "CHAMPIONS7", "g": "Champions"},
     {"n": "M. Liga de Campeones 8", "id": "CHAMPIONS8", "g": "Champions"},
-    {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
-    {"n": "LaLiga Hypermotion 2", "id": "HYPERMOTION2", "g": "Fútbol"},
-    
-    # --- MOTOR Y DEPORTES ---
-    {"n": "DAZN F1", "id": "DAZNF1", "g": "Motor"},
+
+    # --- DEPORTES Y MOTOR ---
+    {"n": "DAZN F1", "id": "DAZNF1", "g": "Deportes"},
     {"n": "DAZN 1", "id": "DAZN1", "g": "Deportes"},
     {"n": "DAZN 2", "id": "DAZN2", "g": "Deportes"},
     {"n": "DAZN 3", "id": "DAZN3", "g": "Deportes"},
     {"n": "DAZN 4", "id": "DAZN4", "g": "Deportes"},
     {"n": "M. Deportes", "id": "DEPORTES", "g": "Deportes"},
+    {"n": "M. Deportes 2", "id": "DEPORTES2", "g": "Deportes"},
+    {"n": "M. Golf", "id": "GOLF", "g": "Deportes"},
     {"n": "Eurosport 1", "id": "EUROSPORT1", "g": "Deportes"},
     {"n": "Eurosport 2", "id": "EUROSPORT2", "g": "Deportes"},
+    {"n": "Real Madrid TV", "id": "RMTV", "g": "Deportes"},
 
-    # --- TDT ESPAÑA COMPLETA ---
-    {"n": "La 1", "id": "TVE", "g": "TDT"},
-    {"n": "La 2", "id": "TVE2", "g": "TDT"},
-    {"n": "Antena 3", "id": "ANTENA3", "g": "TDT"},
-    {"n": "Cuatro", "id": "CUATRO", "g": "TDT"},
-    {"n": "Telecinco", "id": "TELECINCO", "g": "TDT"},
-    {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
-    {"n": "FDF", "id": "FDF", "g": "TDT"},
-    {"n": "Energy", "id": "ENERGY", "g": "TDT"},
-    {"n": "Mega", "id": "MEGA", "g": "TDT"},
-    {"n": "Neox", "id": "NEOX", "g": "TDT"},
-    {"n": "Nova", "id": "NOVA", "g": "TDT"},
-    {"n": "Atreseries", "id": "ATRESERIES", "g": "TDT"},
-    {"n": "GOL PLAY", "id": "GOL", "g": "TDT"},
-
-    # --- CINE, SERIES Y ADULTOS ---
+    # --- PELÍCULAS Y SERIES ---
     {"n": "M. Estrenos", "id": "ESTRENOS", "g": "Cine"},
+    {"n": "M. Estrenos 2", "id": "ESTRENOS2", "g": "Cine"},
     {"n": "M. Acción", "id": "ACCION", "g": "Cine"},
     {"n": "M. Comedia", "id": "COMEDIA", "g": "Cine"},
     {"n": "M. Drama", "id": "DRAMA", "g": "Cine"},
-    {"n": "Warner TV", "id": "WARNERTV", "g": "Series"},
+    {"n": "M. Cine Español", "id": "ESPANOL", "g": "Cine"},
+    {"n": "Warner TV", "id": "WARNERTV", "g": "Cine"},
     {"n": "FOX", "id": "FOX", "g": "Series"},
+    {"n": "AXN", "id": "AXN", "g": "Series"},
+    {"n": "Calle 13", "id": "CALLE13", "g": "Series"},
+    {"n": "SyFy", "id": "SYFY", "g": "Series"},
+    {"n": "Cosmo", "id": "COSMO", "g": "Series"},
+    {"n": "Comedy Central", "id": "COMEDY", "g": "Series"},
+
+    # --- ADULTOS ---
     {"n": "Playboy TV", "id": "PLAYBOY", "g": "Adultos"},
-    {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"}
+    {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"},
+    {"n": "Hustler TV", "id": "HUSTLER", "g": "Adultos"},
+    {"n": "Penthouse Gold", "id": "PENTHOUSE", "g": "Adultos"}
 ]
 
 @app.route("/playlist.m3u8")
 def playlist():
-    m3u = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/davidmuma/EPG_Movistar/master/guide.xml"\n'
-    ua = "|User-Agent=AppleTV6,2/18L203|Referer=https://ver.movistarplus.es/"
+    m3u = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/davidmuma/EPG_Movistar/master/guide.xml"\n\n'
     
-    for c in canales_data:
+    # Este es el User-Agent exacto que usa tu lista de Dropbox y que evita el 0% de buffer
+    ua = "|http-user-agent=Mozilla/5.0 (QtEmbedded; Linux; Qt 4.8.1) AppleWebKit/534.34 (KHTML, like Gecko) Safari/534.34"
+    
+    for c in CANALES:
         m3u += f'#EXTINF:-1 tvg-id="{c["id"]}" group-title="{c["g"]}", {c["n"]}\n'
         m3u += f'https://ver.movistarplus.es/apple/live/{c["id"]}/{c["id"]}.m3u8{ua}\n'
     

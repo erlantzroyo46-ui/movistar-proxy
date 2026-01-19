491a2ea6ded1b02e0e7dbb63b7ca7f9dbf4c7c07
erlantzroyo46-ui <erlantzroyo46@gmail.com>
2026-01-19 10:25:15 +0100

    app.py

--- app.py
+++ app.py
@@ -5,54 +5,81 @@ app = Flask(__name__)
 
 @app.route("/playlist.m3u8")
 def playlist():
-    # Cabecera con EPG para ver la programación en la tele
     m3u = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/davidmuma/EPG_Movistar/master/guide.xml"\n'
-    
-    # El "secreto" de la lista perfecta: User-Agent pegado al enlace
     ua = "|User-Agent=AppleTV6,2/18L203"
     
-    # LISTA MASIVA COMPLETA (PAQUETE PREMIUM)
     canales = [
-        # --- MOTOR Y DEPORTES ---
+        # --- FÚTBOL TOTAL (LALIGA, CHAMPIONS, SEGUNDA) ---
+        {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
+        {"n": "M. LALIGA TV 2", "id": "LALIGA2", "g": "Fútbol"},
+        {"n": "M. LALIGA TV 3", "id": "LALIGA3", "g": "Fútbol"},
+        {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
+        {"n": "DAZN LALIGA 2", "id": "DAZNLALIGA2", "g": "Fútbol"},
+        {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
+        {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
+        {"n": "M. Liga de Campeones 3", "id": "CHAMPIONS3", "g": "Champions"},
+        {"n": "M. Liga de Campeones 4", "id": "CHAMPIONS4", "g": "Champions"},
+        {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
+        {"n": "LaLiga Hypermotion 2", "id": "HYPERMOTION2", "g": "Fútbol"},
+        {"n": "Copa del Rey", "id": "COPA", "g": "Fútbol"},
+        
+        # --- DEPORTES Y MOTOR ---
         {"n": "DAZN F1", "id": "DAZNF1", "g": "Motor"},
         {"n": "DAZN 1", "id": "DAZN1", "g": "Deportes"},
         {"n": "DAZN 2", "id": "DAZN2", "g": "Deportes"},
         {"n": "M. Deportes", "id": "DEPORTES", "g": "Deportes"},
+        {"n": "M. Deportes 2", "id": "DEPORTES2", "g": "Deportes"},
+        {"n": "M. Golf", "id": "GOLF", "g": "Deportes"},
         {"n": "Eurosport 1", "id": "EUROSPORT1", "g": "Deportes"},
-        
-        # --- FÚTBOL (LALIGA Y SEGUNDA) ---
-        {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
-        {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
-        {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
-        
-        # --- CHAMPIONS Y EUROPA LEAGUE ---
-        {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
-        {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
-        
-        # --- TDT ESPAÑA COMPLETO ---
+        {"n": "Eurosport 2", "id": "EUROSPORT2", "g": "Deportes"},
+
+        # --- TDT ESPAÑA COMPLETA ---
         {"n": "La 1", "id": "TVE", "g": "TDT"},
+        {"n": "La 2", "id": "TVE2", "g": "TDT"},
         {"n": "Antena 3", "id": "ANTENA3", "g": "TDT"},
         {"n": "Cuatro", "id": "CUATRO", "g": "TDT"},
         {"n": "Telecinco", "id": "TELECINCO", "g": "TDT"},
         {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
         {"n": "FDF", "id": "FDF", "g": "TDT"},
-        {"n": "Mega", "id": "MEGA", "g": "TDT"},
         {"n": "Energy", "id": "ENERGY", "g": "TDT"},
-        
+        {"n": "Divinity", "id": "DIVINITY", "g": "TDT"},
+        {"n": "Mega", "id": "MEGA", "g": "TDT"},
+        {"n": "BeMad", "id": "BEMAD", "g": "TDT"},
+        {"n": "Neox", "id": "NEOX", "g": "TDT"},
+        {"n": "Nova", "id": "NOVA", "g": "TDT"},
+        {"n": "Atreseries", "id": "ATRESERIES", "g": "TDT"},
+        {"n": "Trece", "id": "TRECE", "g": "TDT"},
+        {"n": "DMAX", "id": "DMAX", "g": "TDT"},
+        {"n": "DKISS", "id": "DKISS", "g": "TDT"},
+        {"n": "GOL PLAY", "id": "GOL", "g": "TDT"},
+        {"n": "Disney Channel", "id": "DISNEY", "g": "Infantil"},
+        {"n": "Boing", "id": "BOING", "g": "Infantil"},
+        {"n": "Clan", "id": "CLAN", "g": "Infantil"},
+
         # --- CINE Y SERIES ---
         {"n": "M. Estrenos", "id": "ESTRENOS", "g": "Cine"},
+        {"n": "M. Estrenos 2", "id": "ESTRENOS2", "g": "Cine"},
         {"n": "M. Acción", "id": "ACCION", "g": "Cine"},
+        {"n": "M. Comedia", "id": "COMEDIA", "g": "Cine"},
+        {"n": "M. Drama", "id": "DRAMA", "g": "Cine"},
+        {"n": "M. Cine Español", "id": "ESPANOL", "g": "Cine"},
         {"n": "Warner TV", "id": "WARNERTV", "g": "Series"},
         {"n": "FOX", "id": "FOX", "g": "Series"},
-        
+        {"n": "AXN", "id": "AXN", "g": "Series"},
+        {"n": "AXN Movies", "id": "AXNMOVIES", "g": "Series"},
+        {"n": "Calle 13", "id": "CALLE13", "g": "Series"},
+        {"n": "SyFy", "id": "SYFY", "g": "Series"},
+        {"n": "Cosmo", "id": "COSMO", "g": "Series"},
+
         # --- ADULTOS ---
         {"n": "Playboy TV", "id": "PLAYBOY", "g": "Adultos"},
-        {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"}
+        {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"},
+        {"n": "Hustler TV", "id": "HUSTLER", "g": "Adultos"},
+        {"n": "Penthouse Gold", "id": "PENTHOUSE", "g": "Adultos"}
     ]
     
     for c in canales:
         m3u += f'#EXTINF:-1 tvg-id="{c["id"]}" group-title="{c["g"]}", {c["n"]}\n'
-        # Esto envía a la tele directamente al servidor de origen con la identidad correcta
         m3u += f'https://ver.movistarplus.es/apple/live/{c["id"]}/{c["id"]}.m3u8{ua}\n'
     
     return Response(m3u, mimetype='application/x-mpegurl')

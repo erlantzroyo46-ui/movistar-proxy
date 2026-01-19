14bc3a36afb7e16d8c473ada58023b18bfe5b877
erlantzroyo46-ui <erlantzroyo46@gmail.com>
2026-01-19 10:39:44 +0100

    app.py

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from flask import Flask, Response, request
+from flask import Flask, Response
 import os
 
 app = Flask(__name__)
 
 @app.route("/playlist.m3u8")
 def playlist():
+    # El secreto de la lista "perfecta" es usar 'http-user-agent' en lugar de 'User-Agent'
+    ua = "|http-user-agent=AppleTV6,2/18L203"
+    
     m3u = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/davidmuma/EPG_Movistar/master/guide.xml"\n'
-    ua = "|User-Agent=AppleTV6,2/18L203"
     
     canales = [
-        # --- FÚTBOL TOTAL (LALIGA, CHAMPIONS, SEGUNDA) ---
+        # --- FÚTBOL Y CHAMPIONS ---
         {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
-        {"n": "M. LALIGA TV 2", "id": "LALIGA2", "g": "Fútbol"},
-        {"n": "M. LALIGA TV 3", "id": "LALIGA3", "g": "Fútbol"},
         {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
-        {"n": "DAZN LALIGA 2", "id": "DAZNLALIGA2", "g": "Fútbol"},
         {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
         {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
         {"n": "M. Liga de Campeones 3", "id": "CHAMPIONS3", "g": "Champions"},
-        {"n": "M. Liga de Campeones 4", "id": "CHAMPIONS4", "g": "Champions"},
         {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
-        {"n": "LaLiga Hypermotion 2", "id": "HYPERMOTION2", "g": "Fútbol"},
         {"n": "Copa del Rey", "id": "COPA", "g": "Fútbol"},
         
-        # --- DEPORTES Y MOTOR ---
+        # --- MOTOR Y DEPORTES ---
         {"n": "DAZN F1", "id": "DAZNF1", "g": "Motor"},
         {"n": "DAZN 1", "id": "DAZN1", "g": "Deportes"},
         {"n": "DAZN 2", "id": "DAZN2", "g": "Deportes"},
-        {"n": "M. Deportes", "id": "DEPORTES", "g": "Deportes"},
-        {"n": "M. Deportes 2", "id": "DEPORTES2", "g": "Deportes"},
-        {"n": "M. Golf", "id": "GOLF", "g": "Deportes"},
         {"n": "Eurosport 1", "id": "EUROSPORT1", "g": "Deportes"},
         {"n": "Eurosport 2", "id": "EUROSPORT2", "g": "Deportes"},
 
@@ -42,47 +36,28 @@ def playlist():
         {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
         {"n": "FDF", "id": "FDF", "g": "TDT"},
         {"n": "Energy", "id": "ENERGY", "g": "TDT"},
-        {"n": "Divinity", "id": "DIVINITY", "g": "TDT"},
         {"n": "Mega", "id": "MEGA", "g": "TDT"},
-        {"n": "BeMad", "id": "BEMAD", "g": "TDT"},
         {"n": "Neox", "id": "NEOX", "g": "TDT"},
         {"n": "Nova", "id": "NOVA", "g": "TDT"},
-        {"n": "Atreseries", "id": "ATRESERIES", "g": "TDT"},
-        {"n": "Trece", "id": "TRECE", "g": "TDT"},
-        {"n": "DMAX", "id": "DMAX", "g": "TDT"},
-        {"n": "DKISS", "id": "DKISS", "g": "TDT"},
-        {"n": "GOL PLAY", "id": "GOL", "g": "TDT"},
-        {"n": "Disney Channel", "id": "DISNEY", "g": "Infantil"},
-        {"n": "Boing", "id": "BOING", "g": "Infantil"},
-        {"n": "Clan", "id": "CLAN", "g": "Infantil"},
+        {"n": "BeMad", "id": "BEMAD", "g": "TDT"},
 
         # --- CINE Y SERIES ---
         {"n": "M. Estrenos", "id": "ESTRENOS", "g": "Cine"},
-        {"n": "M. Estrenos 2", "id": "ESTRENOS2", "g": "Cine"},
         {"n": "M. Acción", "id": "ACCION", "g": "Cine"},
-        {"n": "M. Comedia", "id": "COMEDIA", "g": "Cine"},
-        {"n": "M. Drama", "id": "DRAMA", "g": "Cine"},
-        {"n": "M. Cine Español", "id": "ESPANOL", "g": "Cine"},
         {"n": "Warner TV", "id": "WARNERTV", "g": "Series"},
         {"n": "FOX", "id": "FOX", "g": "Series"},
-        {"n": "AXN", "id": "AXN", "g": "Series"},
-        {"n": "AXN Movies", "id": "AXNMOVIES", "g": "Series"},
         {"n": "Calle 13", "id": "CALLE13", "g": "Series"},
-        {"n": "SyFy", "id": "SYFY", "g": "Series"},
-        {"n": "Cosmo", "id": "COSMO", "g": "Series"},
 
         # --- ADULTOS ---
         {"n": "Playboy TV", "id": "PLAYBOY", "g": "Adultos"},
-        {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"},
-        {"n": "Hustler TV", "id": "HUSTLER", "g": "Adultos"},
-        {"n": "Penthouse Gold", "id": "PENTHOUSE", "g": "Adultos"}
+        {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"}
     ]
     
     for c in canales:
         m3u += f'#EXTINF:-1 tvg-id="{c["id"]}" group-title="{c["g"]}", {c["n"]}\n'
         m3u += f'https://ver.movistarplus.es/apple/live/{c["id"]}/{c["id"]}.m3u8{ua}\n'
     
-    return Response(m3u, mimetype='application/x-mpegurl')
+    return Response(m3u, mimetype='application/octet-stream')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))

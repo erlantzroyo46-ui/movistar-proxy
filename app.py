d3b5bdf5044daeb67e3b777833adb40e3f90a116
erlantzroyo46-ui <erlantzroyo46@gmail.com>
2026-01-19 11:53:04 +0100

    app.py

--- app.py
+++ app.py
@@ -3,59 +3,67 @@ import os
 
 app = Flask(__name__)
 
+# LISTA MASIVA COMPLETA - PAQUETE PREMIUM TOTAL
+canales_data = [
+    # --- FÚTBOL (LALIGA, CHAMPIONS, MULTIS) ---
+    {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
+    {"n": "M. LALIGA TV 2", "id": "LALIGA2", "g": "Fútbol"},
+    {"n": "M. LALIGA TV 3", "id": "LALIGA3", "g": "Fútbol"},
+    {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
+    {"n": "DAZN LALIGA 2", "id": "DAZNLALIGA2", "g": "Fútbol"},
+    {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
+    {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
+    {"n": "M. Liga de Campeones 3", "id": "CHAMPIONS3", "g": "Champions"},
+    {"n": "M. Liga de Campeones 4", "id": "CHAMPIONS4", "g": "Champions"},
+    {"n": "M. Liga de Campeones 5", "id": "CHAMPIONS5", "g": "Champions"},
+    {"n": "M. Liga de Campeones 6", "id": "CHAMPIONS6", "g": "Champions"},
+    {"n": "M. Liga de Campeones 7", "id": "CHAMPIONS7", "g": "Champions"},
+    {"n": "M. Liga de Campeones 8", "id": "CHAMPIONS8", "g": "Champions"},
+    {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
+    {"n": "LaLiga Hypermotion 2", "id": "HYPERMOTION2", "g": "Fútbol"},
+    
+    # --- MOTOR Y DEPORTES ---
+    {"n": "DAZN F1", "id": "DAZNF1", "g": "Motor"},
+    {"n": "DAZN 1", "id": "DAZN1", "g": "Deportes"},
+    {"n": "DAZN 2", "id": "DAZN2", "g": "Deportes"},
+    {"n": "DAZN 3", "id": "DAZN3", "g": "Deportes"},
+    {"n": "DAZN 4", "id": "DAZN4", "g": "Deportes"},
+    {"n": "M. Deportes", "id": "DEPORTES", "g": "Deportes"},
+    {"n": "Eurosport 1", "id": "EUROSPORT1", "g": "Deportes"},
+    {"n": "Eurosport 2", "id": "EUROSPORT2", "g": "Deportes"},
+
+    # --- TDT ESPAÑA COMPLETA ---
+    {"n": "La 1", "id": "TVE", "g": "TDT"},
+    {"n": "La 2", "id": "TVE2", "g": "TDT"},
+    {"n": "Antena 3", "id": "ANTENA3", "g": "TDT"},
+    {"n": "Cuatro", "id": "CUATRO", "g": "TDT"},
+    {"n": "Telecinco", "id": "TELECINCO", "g": "TDT"},
+    {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
+    {"n": "FDF", "id": "FDF", "g": "TDT"},
+    {"n": "Energy", "id": "ENERGY", "g": "TDT"},
+    {"n": "Mega", "id": "MEGA", "g": "TDT"},
+    {"n": "Neox", "id": "NEOX", "g": "TDT"},
+    {"n": "Nova", "id": "NOVA", "g": "TDT"},
+    {"n": "Atreseries", "id": "ATRESERIES", "g": "TDT"},
+    {"n": "GOL PLAY", "id": "GOL", "g": "TDT"},
+
+    # --- CINE, SERIES Y ADULTOS ---
+    {"n": "M. Estrenos", "id": "ESTRENOS", "g": "Cine"},
+    {"n": "M. Acción", "id": "ACCION", "g": "Cine"},
+    {"n": "M. Comedia", "id": "COMEDIA", "g": "Cine"},
+    {"n": "M. Drama", "id": "DRAMA", "g": "Cine"},
+    {"n": "Warner TV", "id": "WARNERTV", "g": "Series"},
+    {"n": "FOX", "id": "FOX", "g": "Series"},
+    {"n": "Playboy TV", "id": "PLAYBOY", "g": "Adultos"},
+    {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"}
+]
+
 @app.route("/playlist.m3u8")
 def playlist():
     m3u = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/davidmuma/EPG_Movistar/master/guide.xml"\n'
-    
-    # Inyectamos el Referer para que Movistar crea que vienes de su web oficial
     ua = "|User-Agent=AppleTV6,2/18L203|Referer=https://ver.movistarplus.es/"
     
-    canales = [
-        # --- FÚTBOL TOTAL (LALIGA, CHAMPIONS, MULTIS) ---
-        {"n": "M. LALIGA TV", "id": "LALIGA", "g": "Fútbol"},
-        {"n": "M. LALIGA TV 2", "id": "LALIGA2", "g": "Fútbol"},
-        {"n": "M. LALIGA TV 3", "id": "LALIGA3", "g": "Fútbol"},
-        {"n": "DAZN LALIGA", "id": "DAZNLALIGA", "g": "Fútbol"},
-        {"n": "DAZN LALIGA 2", "id": "DAZNLALIGA2", "g": "Fútbol"},
-        {"n": "M. Liga de Campeones", "id": "CHAMPIONS", "g": "Champions"},
-        {"n": "M. Liga de Campeones 2", "id": "CHAMPIONS2", "g": "Champions"},
-        {"n": "M. Liga de Campeones 3", "id": "CHAMPIONS3", "g": "Champions"},
-        {"n": "M. Liga de Campeones 4", "id": "CHAMPIONS4", "g": "Champions"},
-        {"n": "M. Liga de Campeones 5", "id": "CHAMPIONS5", "g": "Champions"},
-        {"n": "LaLiga Hypermotion", "id": "HYPERMOTION", "g": "Fútbol"},
-        {"n": "Copa del Rey", "id": "COPA", "g": "Fútbol"},
-        
-        # --- MOTOR Y DEPORTES ---
-        {"n": "DAZN F1", "id": "DAZNF1", "g": "Motor"},
-        {"n": "DAZN 1", "id": "DAZN1", "g": "Deportes"},
-        {"n": "DAZN 2", "id": "DAZN2", "g": "Deportes"},
-        {"n": "M. Deportes", "id": "Deportes"},
-        {"n": "Eurosport 1", "id": "EUROSPORT1", "g": "Deportes"},
-        {"n": "Eurosport 2", "id": "EUROSPORT2", "g": "Deportes"},
-
-        # --- TDT ESPAÑA COMPLETA ---
-        {"n": "La 1", "id": "TVE", "g": "TDT"},
-        {"n": "La 2", "id": "TVE2", "g": "TDT"},
-        {"n": "Antena 3", "id": "ANTENA3", "g": "TDT"},
-        {"n": "Cuatro", "id": "CUATRO", "g": "TDT"},
-        {"n": "Telecinco", "id": "TELECINCO", "g": "TDT"},
-        {"n": "La Sexta", "id": "LASEXTA", "g": "TDT"},
-        {"n": "FDF", "id": "FDF", "g": "TDT"},
-        {"n": "Energy", "id": "ENERGY", "g": "TDT"},
-        {"n": "Mega", "id": "MEGA", "g": "TDT"},
-        {"n": "Neox", "id": "NEOX", "g": "TDT"},
-        {"n": "Nova", "id": "NOVA", "g": "TDT"},
-
-        # --- CINE, SERIES Y ADULTOS ---
-        {"n": "M. Estrenos", "id": "ESTRENOS", "g": "Cine"},
-        {"n": "M. Acción", "id": "ACCION", "g": "Cine"},
-        {"n": "Warner TV", "id": "WARNERTV", "g": "Series"},
-        {"n": "FOX", "id": "FOX", "g": "Series"},
-        {"n": "Playboy TV", "id": "PLAYBOY", "g": "Adultos"},
-        {"n": "Vivid Red", "id": "VIVIDRED", "g": "Adultos"}
-    ]
-    
-    for c in canales:
+    for c in canales_data:
         m3u += f'#EXTINF:-1 tvg-id="{c["id"]}" group-title="{c["g"]}", {c["n"]}\n'
         m3u += f'https://ver.movistarplus.es/apple/live/{c["id"]}/{c["id"]}.m3u8{ua}\n'
     
